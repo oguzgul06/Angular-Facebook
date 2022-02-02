cf5f97c9e5645a2c96e5e3bcb3aa1ee86e87e04c
Oğuz GÜL <ogzgl06@gmail.com>
2022-02-02 20:29:09 +0300

    Initialize Project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//https://www.youtube.com/watch?v=XQA1bbHQJp0&t=1136s (10.52 dk)
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [],
+  declarations: [],
+  providers: [],
+})
+export class AppRoutingModule {}
