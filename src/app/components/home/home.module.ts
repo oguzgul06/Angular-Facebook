9ed3588409b2f059540915c55a0c83b54ae5d0a2
Oğuz GÜL <ogzgl06@gmail.com>
2022-02-04 13:06:40 +0300

    Build home_body_posts from database

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MaterialModule } from 'src/app/shared/material-flag/material.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, FormsModule],
   exports: [],
   declarations: [HomeComponent],
   providers: [],
