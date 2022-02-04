2edc9556deb4f8edaec2c484a6e6eee01d3595b8
Oğuz GÜL <ogzgl06@gmail.com>
2022-02-04 16:30:37 +0300

    Finish Home_Body design

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,11 +9,11 @@ import { NgForm } from '@angular/forms';
 export class HomeComponent implements OnInit {
   images: any[] = [
     'https://images-na.ssl-images-amazon.com/images/I/51DR2KzeGBL._AC_.jpg',
-    'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg',
-    'https://torange.biz/photofx/93/8/light-vivid-colors-fragment-love-background-rain-fall-cover-93412.jpg',
-    'https://cdn.pixabay.com/photo/2017/07/18/18/24/dove-2516641_960_720.jpg',
-    'https://c0.wallpaperflare.com/preview/956/761/225/5be97da101a3f.jpg',
-    'https://upload.wikimedia.org/wikipedia/commons/9/9a/Swepac_FB_465%2C_RV70%2C_with_passing_lorry.jpg',
+    'https://phrase.com/blog/content/uploads/2016/09/javascript-localization.png',
+    'https://m.media-amazon.com/images/I/5160dwNeqSL._AC_SY1000_.jpg',
+    'https://blog.logrocket.com/wp-content/uploads/2022/01/mapped-types-typescript.png',
+    'https://images.indepth.dev/images/2021/03/Global-objects-in-Angular.jpeg',
+    'https://media.bitdegree.org/storage/media/images/2018/10/The-Most-Essential-React-Interview-Questions.jpg',
   ];
 
   constructor() {}
