4961f650af9c28aed30b32c6d2c01891623f6ce2
Oğuz GÜL <ogzgl06@gmail.com>
2022-02-06 13:18:04 +0300

    Build Signup auth-config

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { userData } from 'src/app/services/auth.service';
+import { PostService } from '../../services/post.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   images: any[] = [
     'https://images-na.ssl-images-amazon.com/images/I/51DR2KzeGBL._AC_.jpg',
     'https://phrase.com/blog/content/uploads/2016/09/javascript-localization.png',
@@ -16,9 +20,25 @@ export class HomeComponent implements OnInit {
     'https://media.bitdegree.org/storage/media/images/2018/10/The-Most-Essential-React-Interview-Questions.jpg',
   ];
 
-  constructor() {}
+  posts: any[] = [];
+  user: userData;
+  subs: Subscription[] = [];
 
-  ngOnInit(): void {}
+  constructor(private postservice: PostService, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.postservice.getAllPosts().subscribe((posts) => {
+      this.posts = posts;
+    });
+
+    /* this.subs.push(
+      this.authService
+    ) */
+  }
+
+  ngOnDestroy(): void {
+    this.subs.map((s) => s.unsubscribe());
+  }
 
   postMessage(form: NgForm) {
     console.log(form.value);
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,74 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { BehaviorSubject, Observable } from 'rxjs';
+import * as firebase from 'firebase/app';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  private userData: Observable<firebase.User>;
+
+  private currentUser: UserData;
+  private currentUser$ = new BehaviorSubject<UserData>(null);
+
+  constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth) {
+    this.userData = afAuth.authState;
+    this.userData.subscribe(user => {
+      if (user) {
+        this.afs.collection<UserData>('users').doc<Userdata>(user.uid).valueChanges().subscribe(currentUser=> {
+          if (currentUser !== undefined) {
+            this.currentUser = currentUser;
+            this.currentUser$.next(this.currentUser);
+          }else {
+            this.currentUser = null;
+            this.currentUser$.next(this.currentUser);
+          }
+
+        })
+      }
+    })
+
+
+    CurrentUser(): Observable<UserData>{
+      return this.currentUser$.asObservable();
+    }
+
+
+    SignUp(email:string, password:string,firstname: string, lastname:string, avatar:string='https://pbs.twimg.com/media/EelQUl8XsAAUN-P.jpg'){
+
+      this.afAuth.createUserWithEmailAndPassword(email,password)
+          .then(res=> {
+            if (res) {
+                this.afs.collection('users').doc(res.user.uid)
+                  .set({
+                    firstname,
+                    lastname,
+                    email,
+                    avatar
+                  }).then(()=> {
+                    this.afs.collection<UserData>('users')
+                        .doc<UserData>(res.user.uid)
+                        .valueChanges()
+                        .subscribe(user=> {
+                          if (user) {
+                            this.currentUser = user;
+                            this.currentUser$.next(this.currentUser);
+                          }
+                        })
+                  }).catch(err => console.log(err));
+            }
+          }).catch(err => console.log(err));
+
+  }
+    export interface userData {
+      firstName: string;
+      lastName: string;
+      avatar: string;
+      email: string;
+      id?: string;
+    }
+}
 }
